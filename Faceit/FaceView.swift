d7310624f1abca6310f62ac61341785a37546f99
zhuwenjie <229657322@qq.com>
2023-06-26 08:59:58 +0800

    Initial Commit

---
+++ Faceit/FaceView.swift
@@ -0,0 +1,20 @@
+//
+//  FaceView.swift
+//  Faceit
+//
+//  Created by 朱文杰 on 2023/6/26.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Faceit/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Faceit
+//
+//  Created by 朱文杰 on 2023/6/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+}
+
